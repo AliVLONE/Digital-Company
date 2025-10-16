2def0127b38f326f89c3f20413443d490731fb15
homow <homowsh897@gmail.com>
2025-10-16 22:43:07 +0330

    config changeTheme

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,18 +1,36 @@
-const themeIcon = document.documentElement.classList.contains("dark")
+function bindEvents() {
+    let theme = null;
+    const localTheme = localStorage.getItem("theme");
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-function changeTheme() {
-    const theme = localStorage.getItem("theme");
+    const themeBtn = document.getElementById("change-theme")
+
+    theme = localTheme === "dark" || (!localTheme && prefersDark) ? "dark" : "light";
+
+    const changeTheme = () => {
+        const newTheme = theme === "dark" ? "light" : "dark";
+        theme = newTheme;
+        document.documentElement.classList.toggle("dark", newTheme === "dark");
+        localStorage.setItem("theme", newTheme);
+    }
+
+    themeBtn.addEventListener("click", changeTheme);
 }
 
 export default function Header() {
     return `
 <div>
-<span onclick="">
-    <svg>
-        <use href=#${themeIcon ? "sun" : "moon"}-icon></use>
+<span id="change-theme" class="cursor-pointer">
+    <svg class="size-8 hidden dark:inline-block">
+        <use href="#sun-icon"></use>
+    </svg>
+    <svg class="size-8 dark:hidden">
+        <use href="#moon-icon"></use>
     </svg>
 </span>
     
     <h1 class="">Header</h1>
 </div>`
 };
+
+export {bindEvents}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "@/styles/global.css";
-import Header from "@layout/Header.js";
+import Header, {bindEvents} from "@layout/Header.js";
 import SvgDefs from "@components/ui/SvgDefs.js";
 
 document.getElementById("header").innerHTML = Header()
 document.getElementById("svg-defs").innerHTML = SvgDefs()
+bindEvents()
