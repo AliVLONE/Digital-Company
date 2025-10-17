219793494234e19e6ebff33ced159a8f28dfb03c
homow <homowsh897@gmail.com>
2025-10-17 23:15:50 +0330

    add SvgDefs dynamic

--- src/includes.js
+++ src/includes.js
@@ -3,6 +3,7 @@ import Header, {bindEventsHeader} from "@layout/Header.js";
 import SvgDefs from "@components/ui/SvgDefs.js";
 
 window.addEventListener("DOMContentLoaded", () => {
+    document.body.insertAdjacentHTML("afterbegin", `<svg class="hidden" id="svg-defs"></svg>`)
     // include header
     document.getElementById("header").innerHTML = Header();
     bindEventsHeader();
