62190b8f4949942cbec2d46c9659cfb88b5f633e
homow <homowsh897@gmail.com>
2025-10-17 23:12:50 +0330

    update include

--- src/includes.js
+++ src/includes.js
@@ -2,9 +2,11 @@ import "@/styles/global.css";
 import Header, {bindEventsHeader} from "@layout/Header.js";
 import SvgDefs from "@components/ui/SvgDefs.js";
 
-// include header
-document.getElementById("header").innerHTML = Header();
-bindEventsHeader();
+window.addEventListener("DOMContentLoaded", () => {
+    // include header
+    document.getElementById("header").innerHTML = Header();
+    bindEventsHeader();
 
-// include svg icons symbol
-document.getElementById("svg-defs").innerHTML = SvgDefs();
+    // include svg icons symbol
+    document.getElementById("svg-defs").innerHTML = SvgDefs();
+});
