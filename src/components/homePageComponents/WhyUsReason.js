b4482df5c3e98f557f53e1f2f179d697fc80782a
homow <homowsh897@gmail.com>
2025-10-19 20:39:21 +0330

    update WhyUsReason

--- src/components/homePageComponents/WhyUsReason.js
+++ src/components/homePageComponents/WhyUsReason.js
@@ -2,10 +2,15 @@ export default function WhyUsReason({icon, title, text}) {
     return `
 <div class="space-y-5">
     <!-- icon wrapper -->
-    <div class="p-3.5 rounded-full border-1 border-grey-30">
-        <svg class="size-6">
-            <use href="#${icon}-icon"></use>
-        </svg>
+    <div class="max-w-max relative rounded-full border-4 border-grey-30 overflow-hidden">
+        <img class="absolute -z-10 inset-0 min-w-70 h-65 opacity-8"
+         src="/src/assets/img/home/Vector.webp" alt="vector">
+         
+         <div class="bg-white/5 size-full p-3.5">
+            <svg class="size-6">
+                <use href="#${icon}-icon"></use>
+            </svg>
+        </div>
     </div>
     
     <!-- info wrapper -->
@@ -15,13 +20,13 @@ export default function WhyUsReason({icon, title, text}) {
     </div>
     
     <!-- link wrapper -->
-    <div class="border border-grey-12 rounded-full pl-4 py-2.5 pr-2.5">
+    <div class="border border-grey-12 rounded-full pl-4 py-2.5 pr-2.5 text-base">
         Learn More
-        <span class="bg-grey-10">
+        <div class="max-w-max bg-grey-10 rounded-full px-3.5 py-1.5">
             <svg class="size-4">
                 <use href="#smallRight-icon"></use>
             </svg>
-        </span>
+        </div>
     </div>
 </div>`
 }
