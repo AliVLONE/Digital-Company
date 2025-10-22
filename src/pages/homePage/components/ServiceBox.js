12419852a0e9261b179cc9071445301a53e862a8
homow <homowsh897@gmail.com>
2025-10-22 21:28:18 +0330

    update ServicesBox UI

--- src/pages/homePage/components/ServiceBox.js
+++ src/pages/homePage/components/ServiceBox.js
@@ -1,34 +1,33 @@
 export default function ServiceBox({icon, title, description}) {
     return `
-<div class="px-6 pt-12 pb-6 border border-grey-50 rounded-5">
+<div class="relative space-y-6 text-center px-6 pt-12 pb-6 border border-grey-50 rounded-5">
+    <img class="-z-10 absolute inset-0 size-full opacity-20" src="/src/assets/img/home/Vector.webp" alt="vector">
 
-    <div class="relative">
-        <img class="-z-10 absolute inset-0 size-full" src="/src/assets/img/home/Vector.webp" alt="vector">
-        
+    <div class="relative"> 
         <!-- icon and cricle borders -->
-        <span class="border border-grey-50 rounded-full p-1.5">
-            <span class="border border-grey-50 rounded-full p-1.5">
-                <span class="border border-grey-50 rounded-full p-1.5">
+        <div class="max-w-max mx-auto bg-grey-12/40 border border-grey-50 rounded-full p-2">
+            <div class="border border-grey-50 rounded-full p-2">
+                <div class="border border-grey-50 rounded-full p-2">
                     <svg class="size-5">
-                        <use id="#${icon}-icon"></use>
+                        <use href="#${icon}-icon"></use>
                     </svg>
-                </span>
-            </span>
-        </span>
+                </div>
+            </div>
+        </div>
     </div>
     
     <div>
         <h3>${title}</h3>
-        <p>${description}</p>
+        <p class="sub-text">${description}</p>
        
-        <div class="flex flex-row items-center gap-2 border border-grey-15 bg-grey-10 rounded-full">
+        <div class="px-5 py-3.5 flex flex-row items-center gap-2 border border-grey-15 bg-grey-10 rounded-full text-sm">
             Learn More
-            <span>
-                <svg>
-                    <use id="#smallRight-icon"></use>
+            <div>
+                <svg class="size-3">
+                    <use href="#smallRight-icon"></use>
                 </svg>
-            </span>
+            </div>
         </div>
     </div>
 </div>`
-}
+};
